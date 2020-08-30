856036029950bfdd07cb4c64ec6479e2480aa86f
Katherine Posada <kposada@vozy.co>
2020-08-30 10:59:31 -0500

    Commit initial

--- src/app/image.service.spec.ts
+++ src/app/image.service.spec.ts
@@ -1,16 +1,34 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ImageService } from './image.service';
 
 describe('ImageService', () => {
   let service: ImageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ImageService);
+    service = new ImageService();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  describe('getImages', () => {
+    it('cuando se llame el metodo entonces debe retornar todas (5) imagenes', () =>{
+      //Arrange (preparacion de la prueba)
+      //Ya se hizo el before  
+      //Act (llamar la cosa que estoy haciendo)
+      let resp = service.getImages();
+      //Assert
+      expect(resp.length).toBe(5);
+    })
+  })
+
+  describe('getImage', () => {
+    it('cuando se envie 3 (que está dentro de la lista) entonces debe retornar este elemento', () =>{
+      const id = 3;
+      let resp = service.getImage(id)
+      expect(resp.brand).toBe('gato');
+      expect(resp.url).toBe('assets/images/gato1.jpg');
+    });
+    it('cuando se envie 7 (que no está dentro de la lista) entonces debe retornar indefinido', () =>{
+      const id = 7;
+      let resp = service.getImage(id)
+      expect(resp).toBeUndefined();
+    })
+  })
 });
