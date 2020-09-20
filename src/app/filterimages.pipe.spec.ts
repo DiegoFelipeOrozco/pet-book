28682912def353f220ef09ba8f252254c2bd744f
Katherine Posada <kposada@vozy.co>
2020-09-20 12:06:00 -0500

    initial commit Kathe

--- src/app/filterimages.pipe.spec.ts
+++ src/app/filterimages.pipe.spec.ts
@@ -1,9 +1,5 @@
 import { FilterimagesPipe } from './filterimages.pipe';
 
-xdescribe('FilterimagesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterimagesPipe();
-    expect(pipe).toBeTruthy();
 describe('FilterimagesPipe', () => {
 	let pipe: FilterimagesPipe;
 
--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GalleryComponent } from './image-gallery.component';
+import { DebugElement } from '@angular/core';
 
-xdescribe('ImageGalleryComponent', () => {
+describe('ImageGalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
+
+  let allButton: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,6 +21,25 @@ xdescribe('ImageGalleryComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
+  // all button
+
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [GalleryComponent]
+    });
+
+    // create component and test fixture
+    fixture = TestBed.createComponent(GalleryComponent);
+
+    // get test component from the fixture
+    component = fixture.componentInstance;
+
+    allButton = fixture.debugElement.query(By.css('button'));
+
+  });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
